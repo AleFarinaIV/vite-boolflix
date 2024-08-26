888ec7d6c29bb0c018aaac0fd2b30b67277bc7c4
AleFarinaIV <Alessandrofarina401@gmail.com>
2024-08-26 12:50:27 +0200

    [UPD] added API and endpoint into store

---
+++ src/store.js
@@ -0,0 +1,8 @@
+import { reactive } from vue
+
+export const store = reactive ({
+    api_key: "?api_key=bbf024ce6479c00488d2e7b78041a3e7",
+    api_url: "https://api.themoviedb.org/3",
+    movie_endpoint: "/search/movie",
+    tv_endpoint: "/search/tv"
+})
